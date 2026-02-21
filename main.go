5d0e3dc6dbd3b423e253262aac3245acd7a59f40
hashinnadin <hashinnadin@gmail.com>
2026-02-21 16:21:29 +0530

    ef

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+
+// 	p1 := 10
+// 	p2 := &p1
+// 	*p2 = 20
+// 	fmt.Println(*p2, p1)
+// }
+
 package main
 
 import "fmt"
 
-func main() {
+type student struct {
+	name string
+	age  int
+}
 
-	p1 := 10
-	p2 := &p1
-	*p2 = 20
-	fmt.Println(*p2, p1)
+func main() {
+	p1 := new(student)
+	p1.name = "hashin"
+	fmt.Println(p1)
+
 }
