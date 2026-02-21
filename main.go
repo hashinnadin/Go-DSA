abf7c53e808d46d91858e6d3cab4117d37809ebe
hashinnadin <hashinnadin@gmail.com>
2026-02-21 16:49:28 +0530

    up

--- main.go
+++ main.go
@@ -10,18 +10,45 @@
 // 	fmt.Println(*p2, p1)
 // }
 
+// package main
+
+// import "fmt"
+
+// type student struct {
+// 	name string
+// 	age  int
+// }
+
+// func main() {
+// 	p1 := new(student)
+// 	p1.name = "hashin"
+// 	// fmt.Println(p1)
+// }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	m1 := make(chan int)
+//     m1[1]="Hashin"
+// }
+
 package main
 
 import "fmt"
 
-type student struct {
-	name string
-	age  int
+func Maximum(arr []int) int {
+	var result int
+	for _, value := range arr {
+		if value > 0 {
+			result = value
+		}
+	}
+	return result
 }
 
 func main() {
-	p1 := new(student)
-	p1.name = "hashin"
+	p1 := Maximum([]int{1, 2, 3, 4, 5, 6})
 	fmt.Println(p1)
-
 }
