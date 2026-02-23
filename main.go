54d5ab77ff053444c2a3caa08786e88d5a638214
hashinnadin <hashinnadin@gmail.com>
2026-02-23 09:57:54 +0530

    up

--- main.go
+++ main.go
@@ -34,21 +34,48 @@
 //     m1[1]="Hashin"
 // }
 
+// package main
+
+// import "fmt"
+
+// func Maximum(arr []int) {
+
+// 	fmt.Println(arr[len(arr)-1])
+// }
+
+// func main() {
+// 	p1 := []int{1, 2, 3, 4, 5, 6}
+// 	Maximum(p1)
+// }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	arr := [5]int{1, 2, 3, 4, 5}
+// 	index := 2
+// 	newValue := 99
+// 	fmt.Println(arr)
+// 	for i := len(arr) - 1; index > i; i-- {
+// 		arr[i] = arr[i-1]
+// 		fmt.Println(i)
+// 	}
+// 	arr[index] = newValue
+// 	fmt.Println(arr)
+// }
+
 package main
 
 import "fmt"
 
-func Maximum(arr []int) int {
-	var result int
-	for _, value := range arr {
-		if value > 0 {
-			result = value
-		}
-	}
-	return result
-}
-
 func main() {
-	p1 := Maximum([]int{1, 2, 3, 4, 5, 6})
-	fmt.Println(p1)
+	str := []string{"Hashin", "nabeel", "shabin"}
+
+	m1 := make(map[string]int)
+
+	for i, value := range str {
+		m1[value] = i
+	}
+	fmt.Println(m1)
 }
