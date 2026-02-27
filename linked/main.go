fb48ce40c80b1a78621ca63e3aed78b388a0cbea
hashinnadin <hashinnadin@gmail.com>
2026-02-27 10:17:13 +0530

    linked llist

--- linked/main.go
+++ linked/main.go
@@ -1,10 +1,31 @@
 package main
 
+import "fmt"
+
 type Node struct {
 	data int
 	next *Node
 }
 
+func printList(head *Node) {
+	current := head
+
+	for current != nil {
+		fmt.Println(current.data)
+		current = current.next
+	}
+}
 func main() {
 
+	d1 := &Node{data: 10}
+	d2 := &Node{data: 20}
+	d3 := &Node{data: 30}
+	d4 := &Node{data: 40}
+
+	d1.next = d2
+	d2.next = d3
+	d3.next = d4
+
+	head := d1
+	printList(head)
 }
